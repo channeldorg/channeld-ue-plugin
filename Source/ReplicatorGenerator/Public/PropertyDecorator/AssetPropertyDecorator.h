75265ed816148ed8c5203a5c466bfac06a13ec85
indiest <indiestian@gmail.com>
2023-08-25 18:54:25 +0800

    Github got silly and lost change when merging from asset-replication branch, so I have to add them manually

--- Source/ReplicatorGenerator/Public/PropertyDecorator/AssetPropertyDecorator.h
+++ Source/ReplicatorGenerator/Public/PropertyDecorator/AssetPropertyDecorator.h
@@ -18,9 +18,9 @@ UObject* NewAsset = ChanneldUtils::GetAssetByRef(&MessageArr[i]);
 if ((*{Declare_PropertyPtr})[i] != NewAsset)
 {
   (*{Declare_PropertyPtr})[i] = NewAsset;
-  if (!bPropChanged)
+  if (!b{Declare_PropertyName}Changed)
   {
-    bPropChanged = true;
+    b{Declare_PropertyName}Changed = true;
   }
 }
 )EOF";
@@ -51,7 +51,7 @@ public:
 	virtual FString GetCode_SetPropertyValueTo(const FString& TargetInstance, const FString& NewStateName, const FString& AfterSetValueCode) override;
 	virtual FString GetCode_SetDeltaStateArrayInner(const FString& PropertyPointer, const FString& FullStateName, const FString& DeltaStateName, bool ConditionFullStateIsNull) override;
 
-	virtual FString GetCode_SetPropertyValueArrayInner(const FString& PropertyPointer, const FString& NewStateName) override;
+	virtual FString GetCode_SetPropertyValueArrayInner(const FString& ArrayPropertyName, const FString& PropertyPointer, const FString& NewStateName) override;
 
 	virtual TArray<FString> GetAdditionalIncludes() override;
 
