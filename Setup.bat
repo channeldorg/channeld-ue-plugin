516304f454520e8533e5c19c31e617aeeef28e19
indiest <indiestian@gmail.com>
2023-09-21 09:31:22 +0800

    - Updated scripts to match channeld release version;

--- Setup.bat
+++ Setup.bat
@@ -1,5 +1,5 @@
 @echo off
-set ChanneldVersion=v0.6.0
+set ChanneldVersion=v0.7.0
 set ChanneldRepoUrl=https://github.com/metaworking/channeld.git
 set WorkspaceDir=%~dp0
 set ChanneldLocalSourceDir=%~dp0Source\ThirdParty\channeld
